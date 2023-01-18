4706310947355388007e71c438d4c3a0cdffb570
lavanyabalam31 <122992812+lavanyabalam31@users.noreply.github.com>
2023-01-18 20:28:01 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,15 @@
+class A 
+{
+	public static void main(String[] args) 
+	{
+		int a=24;
+		if(a%2==0)
+		{
+			System.out.println(a+"is even number");
+		}
+		else
+		{
+			System.out.println(a+"is odd number");
+		}
+	}
+}
